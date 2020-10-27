2fa248e3109ad5e6d94a53e2bb0dc7cbcf9c807b
koji-endo <skyclear.288@gmail.com>
2020-10-27 22:05:05 +0900

    delete unnecessary dirs

--- abu/src/views/Iframe.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <iframe style="width: 100%; height: 100%;" src="https://ft.com" />
-</template>
-<script>
-export default {};
-</script>
--- flask/supro.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-from subprocess import PIPE
-import sys
-proc = subprocess.run("date", shell=True, stdout=PIPE, stderr=PIPE)
-date = sys.stdout.buffer.write(proc.stdout)
-print('STDOUT: {}'.format(date))
