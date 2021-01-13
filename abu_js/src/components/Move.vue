23680eb0573ac977b8a2317a716dba2b23b7fb6c
koji-endo <skyclear.288@gmail.com>
2021-01-13 16:42:39 +0900

    function to edit origin

--- abu_js/src/components/Move.vue
+++ abu_js/src/components/Move.vue
@@ -1,6 +1,6 @@
 <template>
   <v-row class="blue lighten-4">
-    <v-col cols="6">
+    <v-col cols="4">
       <v-text-field
         label="move length"
         v-model.number="moveLength"
@@ -11,12 +11,39 @@
         dense
       />
     </v-col>
-    <v-col cols="6">
+    <v-col cols="5">
       <v-btn @click="move(0)" dark color="blue" :loading="loading">X</v-btn>
       <v-btn @click="move(1)" dark color="indigo" :loading="loading">Y</v-btn>
       <v-btn @click="move(2)" dark color="purple" :loading="loading">Z</v-btn>
       <v-icon @click="moveLength = -moveLength">mdi-plus-minus</v-icon>
     </v-col>
+    <v-col cols="3">
+      {{ absolutePos }}
+      <v-icon @click="dialog.show = true">mdi-pen</v-icon>
+    </v-col>
+    <v-dialog v-model="dialog.show">
+      <v-card>
+        <v-card-title>Fix Absolute Position</v-card-title>
+        <v-card-text>
+          <v-row>
+            <v-col>
+              <v-text-field label="x" type="number" v-model.number="fix[0]" />
+            </v-col>
+            <v-col>
+              <v-text-field label="y" type="number" v-model.number="fix[1]" />
+            </v-col>
+            <v-col>
+              <v-text-field label="z" type="number" v-model.number="fix[2]" />
+            </v-col>
+          </v-row>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer />
+          <v-btn @click="dialog.show = false">Quit</v-btn>
+          <v-btn @click="fixPos" color="red">Fix </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </v-row>
 </template>
 <script>
@@ -29,9 +56,18 @@ export default {
     return {
       moveLength: 0,
       loading: false,
+      absolutePos: [0, 0, 0],
+      dialog: {
+        show: false,
+      },
+      fix: [0, 0, 0],
     };
   },
   methods: {
+    fixPos() {
+      this.dialog.show = false;
+      this.absolutePos = this.fix.slice(); // clone
+    },
     async move(direction) {
       try {
         this.loading = true;
@@ -85,6 +121,7 @@ export default {
           url: "/stage/move",
         });
         console.log(retval.data);
+        this.absolutePos[direction] += this.moveLength;
         this.resultItem = retval.data.retarr;
       } catch (e) {
         console.error(e);
