6bb156f2425b634ad7778680facf0a2254b73ea3
koji-endo <skyclear.288@gmail.com>
2021-03-20 13:12:12 +0900

    remove unnecessary files

--- abu_js/src/main.js
+++ abu_js/src/main.js
@@ -1,12 +1,10 @@
 import Vue from "vue";
-import VueWorker from "vue-worker";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
 
 Vue.config.productionTip = false;
-Vue.use(VueWorker);
 new Vue({
   router,
   store,
